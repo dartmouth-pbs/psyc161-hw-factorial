cb989cc0f52456e98192108569689dce50bc6d03
Vassiki Chauhan <chauhan.vassiki@gmail.com>
2019-01-24 11:42:26 -0500

    modified factorial skeleton

--- factorial.py
+++ factorial.py
@@ -8,16 +8,21 @@ Note:  this is just a skeleton for you to work with.  But it already
 
 def factorial(n):
     # TODO Define your logic for factorial here
-    return # TODO!
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 
 def test_factorial():
+    # Tests the function with value 1
     assert factorial(1) == 1
-    # TODO: add more
+    assert factorial(0) == 1
 
 if __name__ == '__main__':
     # This is a way to determine either file was "executed", so if it was
     # imported (by e.g. pytest) as a library, we should not run code
     # below
-    nconditions = raw_input("Please enter number of conditions: ")
+    nconditions = int(input("Please enter number of conditions: "))
     norders = factorial(nconditions)
-    print("Number of possible trial orders: " + str(norders)
+    print("Number of possible trial orders: " + str(norders))
