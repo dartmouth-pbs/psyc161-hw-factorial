26730cc9dec91acdfeb097d7f7f64e7a553ba931
Emma Templeton <emtempleton@gmail.com>
2019-01-24 15:59:03 -0500

    fixed PEP8 issues

--- factorial.py
+++ factorial.py
@@ -13,15 +13,17 @@ def factorial(n):
         final_answer *= product
     return final_answer
 
+
 def test_factorial():
     assert factorial(1) == 1
     assert factorial(3) == 6
     assert factorial(6) == 720
 
+
 if __name__ == '__main__':
     # This is a way to determine either file was "executed", so if it was
     # imported (by e.g. pytest) as a library, we should not run code
     # below
-    nconditions = int(raw_input("Please enter number of conditions: "))
+    nconditions = int(input("Please enter number of conditions: "))
     norders = factorial(nconditions)
     print("Number of possible trial orders: " + str(norders))
