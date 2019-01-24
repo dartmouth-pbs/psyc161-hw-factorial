c5d23b77c7a1856dde1485ddc115c44cdd6be679
Mohsen Rakhshan <mohsen.rakhshan.gr@dartmouth.edu>
2019-01-24 12:16:31 -0500

    wrote the factorial function

--- factorial.py
+++ factorial.py
@@ -7,17 +7,32 @@ Note:  this is just a skeleton for you to work with.  But it already
 
 
 def factorial(n):
-    # TODO Define your logic for factorial here
-    return # TODO!
+
+    if n == 0 or n < 0:
+
+        outputCalc = 1
+
+    else:
+
+        outputCalc = 1
+
+        for number in range(1, n+1):
+
+            outputCalc = number * outputCalc
+
+    return outputCalc
+
 
 def test_factorial():
     assert factorial(1) == 1
-    # TODO: add more
+    assert factorial(0) == 1
+    assert factorial(-10) == 1
+
 
 if __name__ == '__main__':
     # This is a way to determine either file was "executed", so if it was
     # imported (by e.g. pytest) as a library, we should not run code
     # below
-    nconditions = raw_input("Please enter number of conditions: ")
+    nconditions = int(input("Please enter number of conditions: "))
     norders = factorial(nconditions)
-    print("Number of possible trial orders: " + str(norders)
+    print("Number of possible trial orders: " + str(norders))
