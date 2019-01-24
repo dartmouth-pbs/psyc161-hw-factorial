a0cb2d938f1b1c999dc78cfa8c063daaa1d1933c
devxl <devxl@vivaldi.net>
2019-01-24 11:40:24 -0500

    Wrote factorial function...fixed mixed parantheses bug
    Wrote factorial function...fixed missing parantheses bug

--- factorial.py
+++ factorial.py
@@ -8,16 +8,23 @@ Note:  this is just a skeleton for you to work with.  But it already
 
 def factorial(n):
     # TODO Define your logic for factorial here
-    return # TODO!
+    fact = 0
+    if n == 1:
+        fact = 1
+    else:
+        fact = factorial(n-1) * n
+    return fact  # TODO!
+
 
 def test_factorial():
     assert factorial(1) == 1
     # TODO: add more
 
+
 if __name__ == '__main__':
     # This is a way to determine either file was "executed", so if it was
     # imported (by e.g. pytest) as a library, we should not run code
     # below
-    nconditions = raw_input("Please enter number of conditions: ")
+    nconditions = input("Please enter number of conditions: ")
     norders = factorial(nconditions)
-    print("Number of possible trial orders: " + str(norders)
+    print("Number of possible trial orders: " + str(norders))
